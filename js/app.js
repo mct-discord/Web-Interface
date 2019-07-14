fe7328a75c84e2a17d10ca4231a589f82e5f6855
nielsdewulf <niels.dewulf2@student.howest.be>
2019-07-14 15:19:56 +0200

    Added search

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 'use strict';
+let modules;
 
+const showModules = function(data) {
+  let opt = '';
+  modules = data.modules;
+  for (let [key, value] of Object.entries(data.modules)) {
+    opt += `<div class="c-search-select__opt js-search__opt" search-id="${value}">${key}</div>`;
+  }
+  document.querySelector('.js-search__results').innerHTML = opt;
+};
+const listenToInput = function() {
+  document.querySelector('.js-input').addEventListener('input', function(e) {
+    let opt = '';
+    for (let [key, value] of Object.entries(modules)) {
+      if (key.toLowerCase().includes(e.target.value.toLowerCase()))
+        opt += `<div class="c-search-select__opt js-search__opt" search-id="${value}">${key}</div>`;
+    }
+    document.querySelector('.js-search__results').innerHTML = opt;
+  });
+};
 const listenToFocus = function() {
   document.querySelector('.js-search').addEventListener('click', function(e) {
     document.querySelector('.js-search').classList.add('c-search-select__input--selected');
@@ -20,9 +39,14 @@ const listenToSelect = function() {
     });
   }
 };
+const getModules = function() {
+  handleData('http://localhost:5000/api/v1/modules', showModules);
+};
 const init = function() {
+  getModules();
   listenToFocus();
   listenToSelect();
+  listenToInput();
 };
 
 document.addEventListener('DOMContentLoaded', function() {

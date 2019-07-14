60fdac69ae535fbb29ec6720b5cb9a6006e1264d
nielsdewulf <niels.dewulf2@student.howest.be>
2019-07-14 14:26:47 +0200

    Fix align issue and added js event for focus

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 'use strict';
 
-const init = function() {};
+const listenToFocus = function() {
+  document.querySelector('.js-input').addEventListener('focusin', function(e) {
+    document.querySelector('.js-search').classList.add('c-search-select__input--selected');
+  });
+  document.querySelector('.js-input').addEventListener('focusout', function(e) {
+    document.querySelector('.js-search').classList.remove('c-search-select__input--selected');
+  });
+};
+
+const init = function() {
+  listenToFocus();
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   init();

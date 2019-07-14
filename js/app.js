f9249525759378df7df56930a491494ba6c805e2
nielsdewulf <niels.dewulf2@student.howest.be>
2019-07-14 14:35:16 +0200

    Fixed focus issue

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
 'use strict';
 
 const listenToFocus = function() {
+  document.querySelector('.js-search').addEventListener('click', function(e) {
+    document.querySelector('.js-search').classList.add('c-search-select__input--selected');
+  });
   document.querySelector('.js-input').addEventListener('focusin', function(e) {
     document.querySelector('.js-search').classList.add('c-search-select__input--selected');
   });
-  document.querySelector('.js-input').addEventListener('focusout', function(e) {
-    document.querySelector('.js-search').classList.remove('c-search-select__input--selected');
+  window.addEventListener('click', function(e) {
+    if (e.target != document.querySelector('.js-search') && e.target != document.querySelector('.js-input'))
+      document.querySelector('.js-search').classList.remove('c-search-select__input--selected');
   });
 };
-
+const listenToSelect = function() {
+  let objs = document.querySelectorAll('.js-search__opt');
+  for (let ob of objs) {
+    ob.addEventListener('click', function(e) {
+      console.warn('Clicked');
+    });
+  }
+};
 const init = function() {
   listenToFocus();
+  listenToSelect();
 };
 
 document.addEventListener('DOMContentLoaded', function() {
